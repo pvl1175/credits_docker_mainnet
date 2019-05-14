d73bd90236d30b8c686bd2fd8ada75f9ccc32e0d
pvl1175 <vitaly.popov.75@gmail.com>
2019-05-14 17:37:34 +0300

    update

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm build/Credits_Network_for_Linux_x64_4_2_410.tar.gz
 g++ -pthread source/runner.cpp  -o build/runner
 cp common/* build/
 
-sudo docker build -t credits .
+sudo docker build -t credits_mainnet .
 sudo docker volume create main_db
 sudo docker volume create main_keys
 
@@ -13,10 +13,10 @@ sudo docker volume create main_keys
 #sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
 # run bash
-#sudo docker run -it -p 6000:6000 -p 9090:9090 --mount source=main_db,target=/credits/main_db --mount source=main_keys,target=/credits/main_keys credits:latest bash
+#sudo docker run -it -p 6000:6000 -p 9090:9090 --mount source=main_db,target=/credits/main_db --mount source=main_keys,target=/credits/main_keys credits_mainnet:latest bash
 
 # run node
-#sudo docker run -it -p 6000:6000 -p 9090:9090 --mount source=main_db,target=/credits/main_db --mount source=main_keys,target=/credits/main_keys credits:latest
+#sudo docker run -it -p 6000:6000 -p 9090:9090 --mount source=main_db,target=/credits/main_db --mount source=main_keys,target=/credits/main_keys credits_mainnet:latest
 
 # additional example
 #./client --db-path main_db/ --public-key-file main_keys/public.txt --private-key-file main_keys/private.txt
